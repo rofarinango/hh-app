2d25b975bf76d98d98be9234b5ae6627e51afb63
Rodrigo Farinango <47066093+rofarinango@users.noreply.github.com>
2025-01-31 11:48:00 -0600

    Fix styles to show CloseButton and Logo correctly

--- src/hh/pages/HomePage.jsx
+++ src/hh/pages/HomePage.jsx
@@ -7,7 +7,7 @@ import youtubeAPIConfig from "../../store/youtube_api/config";
 import { skipToken } from "@reduxjs/toolkit/query";
 import Grid from "@mui/material/Grid2"
 import CloseIcon from "@mui/icons-material/Close";
-import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import ReactPlayer from "react-player/lazy";
 
 const queryParameters = {
   channelId: youtubeAPIConfig.channelID,
@@ -18,8 +18,6 @@ const queryParameters = {
 const style = {
   position: 'relative',
   width: '50vw',
-  // maxHeight: '90vh', // Limit modal height to 90% of the viewport
-  // overflowY: 'auto', // Enable vertical scrolling within the modal
   bgcolor: 'primary.main',
   border: '2px solid #000',
   boxShadow: 24,
@@ -104,164 +102,176 @@ export const HomePage = () => {
                 alignItems: 'flex-start',
                 pt: 4,
                 justifyContent: 'center',
-                overflow: 'auto', // Enable scrolling for the entire modal
+                overflow: 'auto',
               }}
             >
-              <Box sx={style}>
+              <Box>
+              <Box sx={{ position: "relative", pt: "56.25%" }}>
                 <Button
-                    onClick={handleModalClose}
-                    sx={{
-                      position: "absolute",
-                      top: "16px",
-                      right: "16px",
-                      backgroundColor: "black",
-                      color: "text.primary",
-                      minWidth: "40px",
-                      minHeight: "40px",
-                      borderRadius: "50%",
-                    }}
-                  >
-                    <CloseIcon />
-                  </Button>
-                <Grid container>
-                  <Box sx={{width: '100%'}}>
-                    <Grid container>
-                      <Grid size={8}>
-                        <CardMedia
-                          component="img"
-                          alt="hh banner"
-                          height="200"
-                          image="../../../hh-logo-large.png" // TODO get banner from API
-                          sx={{objectFit: 'contain'}}
-                        />
-                      </Grid>
+                  onClick={handleModalClose}
+                  sx={{
+                    position: "absolute",
+                    top: "16px",
+                    right: "16px",
+                    backgroundColor: "black",
+                    color: "text.primary",
+                    minWidth: "40px",
+                    minHeight: "40px",
+                    borderRadius: "50%",
+                    zIndex: 3,
+                  }}
+                >
+                  <CloseIcon />
+                </Button>
+                <CardMedia
+                  component="img"
+                  alt="hh banner"
+                  image="../../../hh-logo-large.png"
+                  sx={{
+                    position: "absolute",
+                    bottom: { xs: "8px", md: "16px" },
+                    left: { xs: "32px", md: "32px" },
+                    width: { xs: "150px", sm: "200px", md: "300px" },
+                    height: "auto",
+                    zIndex: 3,
+                  }}
+                />
+                {/* Invisible overlay to block interactions */}
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: "transparent",
+                    zIndex: 2, // Make sure it's above the video
+                  }}
+                />                
+                {/* Video player */}
+                <ReactPlayer
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    pointerEvents: "none",
+                  }}
+                  url="https://www.youtube.com/watch?v=vFHIBiV65Us"
+                  width="100%"
+                  height="100%"
+                  config={{
+                    youtube: {
+                      playerVars: {
+                        rel: 0,
+                        controls: 0,
+                        autoplay: 1,
+                        iv_load_policy: 3,
+                        start: 60,
+                      },
+                    },
+                  }}
+                />
+              </Box>
+                <Box sx={style}>
+                  <Grid container>
+                    
+                    <Grid size={8} sx={{ display: "flex", alignItems: "center" }}>
+                      <Typography variant="h5" id="modal-modal-description" >
+                        Episodios
+                      </Typography>
                     </Grid>
-                  </Box>
-                  <Grid size={8} sx={{ display: "flex", alignItems: "center" }}>
-                    <Typography variant="h5" id="modal-modal-description" >
-                      Episodios
-                    </Typography>
+                    <Grid size={4}>
+                    <FormControl fullWidth>
+                    <Select
+                      labelId="season-select-label"
+                      id="season-select"
+                      value={selectedSeasonId || sortedSeasons[0]?.id}
+                      onChange={(e) => setSelectedSeasonId(e.target.value)}
+                      renderValue={(selected) => {
+                        // Find the selected season by ID
+                        const selectedSeason = sortedSeasons.find((season) => season.id === selected);
+                        // Return only the season title
+                        return (
+                          <Typography sx={{ fontWeight: 'bold' }}>
+                            {selectedSeason?.title || ''}
+                          </Typography>
+                        );
+                      }}
+                    >
+                      {sortedSeasons?.map((season) => (
+                        <MenuItem value={season.id} key={season.id}>
+                          <Typography sx={{ fontWeight: 'bold' }}>{season.title}</Typography>
+                          <Typography component="div">
+                            <Box sx={{ textAlign: 'right', fontSize: '12px', width: '95px' }}>
+                              ({season.totalEpisodes} episodios)
+                            </Box>
+                          </Typography>
+                        </MenuItem>
+                      ))}
+                    </Select>
+                    </FormControl>
+                    </Grid>
+                    {
+                      episodesLoading ? (
+                        <Typography>Loading Episodes...</Typography>
+                      ) : selectedSeasonId && episodesData ? (
+                        <Box sx={{width: '100%'}}>
+                            <List>
+                            {episodesData?.map((episode, index) => (
+                              <Box key={episode.id} sx={{ position: "relative", "&:hover .play-icon": { opacity: 1 } }}>
+                              <ListItem disablePadding>
+                                <ListItemButton>
+                                  <Grid size={1} sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                                    <Typography variant="h6">{index + 1}</Typography>
+                                  </Grid>
+                                  <Grid size={3} sx={{ padding: 1, position: "relative" }}>
+                                    {/* Thumbnail Image */}
+                                    <CardMedia
+                                      component="img"
+                                      alt="thumbnail"
+                                      image={episode.snippet.thumbnails.default ? episode.snippet.thumbnails.default.url : "../../../hh-logo.jpg"}
+                                      sx={{ width: "100%", borderRadius: "4px" }}
+                                    />
+                                    {/* Play Icon (Hidden by Default, Shows on Hover) */}
+                                    <Box
+                                      className="play-icon"
+                                      sx={{
+                                        position: "absolute",
+                                        top: "50%",
+                                        left: "50%",
+                                        transform: "translate(-50%, -50%)",
+                                        opacity: 0,
+                                        transition: "opacity 0.3s ease-in-out",
+                                      }}
+                                    >
+                                      <svg width="75" height="75" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+                                        {/* Outer Circle with Thin Outline & Transparent Fill */}
+                                        <circle cx="12" cy="12" r="10" strokeWidth="0.5" fill="rgba(0, 0, 0, 0.2)" />
+                                        
+                                        {/* Play Triangle (Solid White) */}
+                                        <polygon points="10,8 16,12 10,16" fill="white" />
+                                      </svg>
+                                    </Box>
+                                  </Grid>
+                                  <Grid size={8} sx={{ display: "flex", alignItems: "center", padding: 2 }}>
+                                    <Typography sx={{ fontWeight: "bold" }}>{episode.title}</Typography>
+                                  </Grid>
+                                </ListItemButton>
+                              </ListItem>
+                              <Divider sx={{ bgcolor: "#3f3f3f", height: "2px", width: "100%" }} />
+                            </Box>
+                            ))}
+                            </List>
+                        </Box>
+                      ) : null
+                    }
                   </Grid>
-                  <Grid size={4}>
-                  <FormControl fullWidth>
-                  <Select
-                    labelId="season-select-label"
-                    id="season-select"
-                    value={selectedSeasonId || sortedSeasons[0]?.id}
-                    onChange={(e) => setSelectedSeasonId(e.target.value)} // Update state on change
-                    renderValue={(selected) => {
-                      // Find the selected season by ID
-                      const selectedSeason = sortedSeasons.find((season) => season.id === selected);
-                      // Return only the season title
-                      return (
-                        <Typography sx={{ fontWeight: 'bold' }}>
-                          {selectedSeason?.title || ''}
-                        </Typography>
-                      );
-                    }}
-                  >
-                    {sortedSeasons?.map((season) => (
-                      <MenuItem value={season.id} key={season.id}>
-                        <Typography sx={{ fontWeight: 'bold' }}>{season.title}</Typography>
-                        <Typography component="div">
-                          <Box sx={{ textAlign: 'right', fontSize: '12px', width: '95px' }}>
-                            ({season.totalEpisodes} episodios)
-                          </Box>
-                        </Typography>
-                      </MenuItem>
-                    ))}
-                  </Select>
-                  </FormControl>
-                  </Grid>
-                  {
-                    episodesLoading ? (
-                      <Typography>Loading Episodes...</Typography>
-                    ) : selectedSeasonId && episodesData ? (
-                      <Box sx={{width: '100%'}}>
-                          <List>
-                          {episodesData?.map((episode, index) => (
-                            <Box key={episode.id} sx={{ position: "relative", "&:hover .play-icon": { opacity: 1 } }}>
-                            <ListItem disablePadding>
-                              <ListItemButton>
-                                <Grid size={1} sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                                  <Typography variant="h6">{index + 1}</Typography>
-                                </Grid>
-                                <Grid size={3} sx={{ padding: 1, position: "relative" }}>
-                                  {/* Thumbnail Image */}
-                                  <CardMedia
-                                    component="img"
-                                    alt="thumbnail"
-                                    image={episode.snippet.thumbnails.default ? episode.snippet.thumbnails.default.url : "../../../hh-logo.jpg"}
-                                    sx={{ width: "100%", borderRadius: "4px" }}
-                                  />
-                                  {/* Play Icon (Hidden by Default, Shows on Hover) */}
-                                  <Box
-                                    className="play-icon"
-                                    sx={{
-                                      position: "absolute",
-                                      top: "50%",
-                                      left: "50%",
-                                      transform: "translate(-50%, -50%)",
-                                      opacity: 0,
-                                      transition: "opacity 0.3s ease-in-out",
-                                    }}
-                                  >
-                                    <svg width="75" height="75" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
-                                      {/* Outer Circle with Thin Outline & Transparent Fill */}
-                                      <circle cx="12" cy="12" r="10" strokeWidth="0.5" fill="rgba(0, 0, 0, 0.2)" />
-                                      
-                                      {/* Play Triangle (Solid White) */}
-                                      <polygon points="10,8 16,12 10,16" fill="white" />
-                                    </svg>
-                                  </Box>
-                                </Grid>
-                                <Grid size={8} sx={{ display: "flex", alignItems: "center", padding: 2 }}>
-                                  <Typography sx={{ fontWeight: "bold" }}>{episode.title}</Typography>
-                                </Grid>
-                              </ListItemButton>
-                            </ListItem>
-                            <Divider sx={{ bgcolor: "#3f3f3f", height: "2px", width: "100%" }} />
-                          </Box>
-                          ))}
-                          </List>
-                      </Box>
-                    ) : null
-                  }
-                </Grid>
+                </Box>
               </Box>
             </Modal>
           </Grid>
         </Grid>
-
-        {/* {seasonsLoading ? (
-          <Typography>Loading Seasons...</Typography>
-        ) : (
-          <ul>
-            {seasonsData?.items?.map((season) => (
-              <li key={season.id} onClick={() => onClickSeason(season.id)}>
-                {season.snippet.title}
-              </li>
-            ))}
-          </ul>
-        )}
-
-        {
-          episodesLoading ? (
-            <Typography>Loading Episodes...</Typography>
-          ) : selectedSeasonId && episodesData ? (
-            <div>
-              <Typography variant="h2">Episodios</Typography>
-              <ul>
-                {episodesData.items.map((episode) => (
-                  <li key={episode.id}>{episode.snippet.title}</li>
-                ))}
-              </ul>
-            </div>
-          ) : null
-        } */}
-
       </Grid>
-      
     </>
   )
 }
